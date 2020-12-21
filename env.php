f0e7e94a7d92c4c508ec9d35b993965df6556138
OMNI.PRO <jesus.salazar@omni.pro>
2020-12-20 19:17:03 -0500

    env.pho generico

---
+++ env.php
@@ -0,0 +1,53 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'bdname',
+                'username' => 'bduser',
+                'password' => 'bdpass',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'full_page' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'translate' => 1,
+        'config_webservice' => 1,
+        'compiled_config' => 1
+    ],
+    'cron' => [
+        'enabled' => 0
+    ],
+    'crypt' => [
+        'key' => 'cryptkey'
+    ],
+    'install' => [
+        'date' => 'Tue, 04 May 2020 14:07:48 +0000'
+    ]
+];
